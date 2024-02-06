e2a71f230b507c1cb0787119910c7d26038d557b
Chris <117781333+chrism-la@users.noreply.github.com>
2024-02-05 21:51:47 -0800

    rendered Database to Home component

--- api/config/config.js
+++ api/config/config.js
@@ -1,24 +1,23 @@
 module.exports = {
-  
-  development: {
-    username: "x",
-    password: 'x',
-    database: process.env.DB_NAME,
-    host: "127.0.0.1",
-    dialect: "postgres"
-  },
-  test: {
-    username: "x",
-    password: 'x',
-    database: process.env.DB_NAME,
-    host: "127.0.0.1",
-    dialect: "postgres"
-  },
-  production: {
-    username: "x",
-    password: 'x',
-    database: process.env.DB_NAME,
-    host: "127.0.0.1",
-    dialect: "postgres"
-  }
-}
+    development: {
+        username: 'postgres',
+        password: 'Mireyda2005!',
+        database: 'WearItAgain',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+    },
+    test: {
+        username: 'postgres',
+        password: 'Mireyda2005!',
+        database: 'WearItAgain',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+    },
+    production: {
+        username: 'postgres',
+        password: 'Mireyda2005!',
+        database: 'WearItAgain',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+    },
+};
