0a7586c92310bc04c400aeb920d8a374e1017521
Chris <117781333+chrism-la@users.noreply.github.com>
2024-02-05 21:16:42 -0800

    created first model

---
+++ api/models/items.js
@@ -0,0 +1,34 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Items extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    Items.init(
+        {
+            item_id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            sequelize,
+            modelName: 'Items',
+            tableName: 'items',
+            timestamps: false,
+        }
+    );
+    return Items;
+};
