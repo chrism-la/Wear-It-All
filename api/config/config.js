39ec474058a99eddbf87a069c77a5bfb0d434bb7
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2024-02-05 23:31:13 -0500

    authenticated sequelize

--- api/config/config.js
+++ api/config/config.js
@@ -1,24 +1,24 @@
 module.exports = {
   
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   }
 }
