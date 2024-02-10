0a5e7dfcb10b102810011d6fcb6240916832a586
Chris <117781333+chrism-la@users.noreply.github.com>
2024-02-10 14:51:14 -0800

    finished all controllers

--- api/controllers/items_controllers.js
+++ api/controllers/items_controllers.js
@@ -23,4 +23,69 @@ items.get('/:name', async (req, res) => {
     }
 });
 
+// CREATE route
+
+items.post('/new', async (req, res) => {
+    try {
+        const { name, image } = req.body;
+        const newItem = await Items.create({ name, image });
+        res.status(200).json({
+            message: 'Succesfully inserted a new Item',
+            data: newItem,
+        });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// DELETE route
+items.delete('/:id', async (req, res) => {
+    try {
+        const itemId = req.params.id;
+
+        // Check if the item exists
+        const itemToDelete = await Items.findByPk(itemId);
+        if (!itemToDelete) {
+            return res.status(404).json({ message: 'Item not found' });
+        }
+
+        //
+        await Items.destroy({
+            where: {
+                item_id: itemId,
+            },
+        });
+
+        res.status(200).json({ message: 'Item deleted successfully' });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+// UPDATE route
+items.put('/:id', async (req, res) => {
+    try {
+        const itemId = req.params.id;
+        const { name, image } = req.body;
+        const itemToUpdate = await Items.findByPk(itemId);
+        if (!itemToUpdate) {
+            return res.status(404).json({ message: 'Item not found' });
+        }
+        await Items.update(
+            { name, image },
+            {
+                where: {
+                    item_id: itemId,
+                },
+            }
+        );
+        const updatedItem = await Items.findByPk(itemId);
+        res.status(200).json({
+            message: 'Item updated Successfully',
+            data: updatedItem,
+        });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 module.exports = items;
