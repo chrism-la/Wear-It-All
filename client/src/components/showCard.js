a9e564841807530aaa68fd50e70751ebaf51444b
Chris <117781333+chrism-la@users.noreply.github.com>
2024-02-12 15:11:34 -0800

    configured Buy button on Show view

--- client/src/components/showCard.js
+++ client/src/components/showCard.js
@@ -1,15 +1,15 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function ShowCard() {
+function ShowCard({ imageData }) {
     const handleBuyClick = async () => {
         try {
-            const response = await fetch('http://localhost:3127/items/:id', {
+            const response = await fetch(`http://localhost:3127/items/${imageData.item_id}`, {
                 method: 'DELETE',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ item_id: 'item_id' }),
+                body: JSON.stringify({ item_id: imageData.item_id }),
             });
             if (response.ok) {
                 console.log('Item deleted successfully');
@@ -23,12 +23,9 @@ function ShowCard() {
 
     return (
         <Card style={{ width: '18rem' }}>
-            <Card.Img
-                variant="top"
-                src="https://images.unsplash.com/photo-1584030373081-f37b7bb4fa8e?q=80&w=991&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            />
+            <Card.Img variant="top" src={imageData.image} alt={imageData.image} />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>{imageData.name}</Card.Title>
                 <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
                 <Button variant="primary" onClick={handleBuyClick}>
                     Buy
--- client/src/views/show.js
+++ client/src/views/show.js
@@ -1,15 +1,18 @@
 // folder views - file show
-import React from 'react'
-import Header from '../components/header'
+import React from 'react';
+import Header from '../components/header';
 import ShowCard from '../components/showCard';
-
-
+import { useLocation } from 'react-router-dom';
 
 function Show() {
+    const location = useLocation();
+
+    const imageData = location.state?.imageData || {};
+
     return (
         <div>
-            <Header/>
-            <ShowCard/>
+            <Header />
+            <ShowCard imageData={imageData} />
         </div>
     );
 }
