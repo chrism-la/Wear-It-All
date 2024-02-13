03fe593d53e88994356ecfef9d38afca223cc9a3
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2024-02-13 00:08:46 -0500

    added update button and form functionality

--- api/config/config.js
+++ api/config/config.js
@@ -1,21 +1,21 @@
 module.exports = {
     development: {
         username: 'postgres',
-        password: 'Mireyda2005!',
+        password: 'Baseball1027!',
         database: 'WearItAgain',
         host: '127.0.0.1',
         dialect: 'postgres',
     },
     test: {
         username: 'postgres',
-        password: 'Mireyda2005!',
+        password: '',
         database: 'WearItAgain',
         host: '127.0.0.1',
         dialect: 'postgres',
     },
     production: {
         username: 'postgres',
-        password: 'Mireyda2005!',
+        password: '',
         database: 'WearItAgain',
         host: '127.0.0.1',
         dialect: 'postgres',
--- client/src/components/showCard.js
+++ client/src/components/showCard.js
@@ -1,8 +1,10 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import '../App.css';
+import { useNavigate } from 'react-router-dom';
 
 function ShowCard({ imageData }) {
+    const navigate = useNavigate();
     const handleBuyClick = async () => {
         try {
             const response = await fetch(`http://localhost:3127/items/${imageData.item_id}`, {
@@ -21,6 +23,10 @@ function ShowCard({ imageData }) {
             console.error('Error deleting item:', error);
         }
     };
+
+    const handleCardClick = (imageData) => {
+        navigate('/update', { state: { imageData }});
+    };
     return (
         <Card className="row justify-content-center mx-auto w-50 min-vw-50 p-2 ">
             <Card.Img variant="top" src={imageData.image} alt={imageData.image} />
@@ -31,7 +37,7 @@ function ShowCard({ imageData }) {
                 <Button type="" className="" onClick={handleBuyClick}>
                     Buying
                 </Button>
-                <Button type="" className="">
+                <Button type="" className="" onClick={() => handleCardClick(imageData)}>
                     Update
                 </Button>
             </Card.Body>
