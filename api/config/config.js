206b9ede83200af4d3b555af043aed1ce0414313
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2024-02-12 21:52:15 -0800

    editing branch

--- api/config/config.js
+++ api/config/config.js
@@ -1,35 +1,23 @@
 module.exports = {
-    development: {
-        username: 'postgres',
-<<<<<<<<< Temporary merge branch 1
-        password: 'Baseball1027!',
-=========
-        password: 'new_password',
->>>>>>>>> Temporary merge branch 2
-        database: 'WearItAgain',
-        host: '127.0.0.1',
-        dialect: 'postgres',
-    },
-    test: {
-        username: 'postgres',
-<<<<<<<<< Temporary merge branch 1
-        password: '',
-=========
-        password: 'new_password',
->>>>>>>>> Temporary merge branch 2
-        database: 'WearItAgain',
-        host: '127.0.0.1',
-        dialect: 'postgres',
-    },
-    production: {
-        username: 'postgres',
-<<<<<<<<< Temporary merge branch 1
-        password: '',
-=========
-        password: 'new_password',
->>>>>>>>> Temporary merge branch 2
-        database: 'WearItAgain',
-        host: '127.0.0.1',
-        dialect: 'postgres',
-    },
+        development: {
+                username: "postgres",
+                password: "new_password",
+                database: "WearItAgain",
+                host: "127.0.0.1",
+                dialect: "postgres",
+        },
+        test: {
+                username: "postgres",
+                password: "new_password",
+                database: "WearItAgain",
+                host: "127.0.0.1",
+                dialect: "postgres",
+        },
+        production: {
+                username: "postgres",
+                password: "new_password",
+                database: "WearItAgain",
+                host: "127.0.0.1",
+                dialect: "postgres",
+        },
 };
